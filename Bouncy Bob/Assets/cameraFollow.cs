ae996dd351d9bd2e65364c5560c4f27e11e1039d
alpinerum <35735448+alpinerum@users.noreply.github.com>
2020-04-22 03:17:22 -0400

    added a lot more stuff

---
+++ Bouncy Bob/Assets/cameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollow : MonoBehaviour
+{
+    public GameObject player;
+    public float cameraHeight = 15f;
+    public float cameraAdjust = 20f;
+    public float cameraOffset = 50f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = player.transform.position;
+        pos.y = cameraHeight;
+        pos.z -= cameraOffset;
+        pos.x += cameraAdjust;
+        transform.position = pos;
+    }
+}
---
+++ Bouncy Bob/Assets/playerMovement.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float bounce = 10f;
+    public float gravity = -9.8f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.1f;
+    public LayerMask groundMask;
+
+    public GameObject Cap;
+
+    bool isLeftGrounded, isRightGrounded, isCentreGrounded;
+    Vector3 velocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        //Debug.Log (isGrounded);
+        //Debug.Log(velocity.y);
+        isLeftGrounded = GameObject.Find("left cap").transform.position.y < 2.3;
+        isRightGrounded = GameObject.Find("right cap").transform.position.y < 2.3;
+        isCentreGrounded = GameObject.Find("Centre").transform.position.y < 2.3;
+        var redCylinder = GameObject.Find("Red Cylinder").GetComponent<Rigidbody>().velocity;
+        var angle = Mathf.Atan2(redCylinder.y, redCylinder.z) * Mathf.Rad2Deg;
+        Debug.Log(Mathf.Cos(89));
+        Debug.Log(Mathf.Cos(89*Mathf.Deg2Rad));
+        //float xAngle = GameObject.Find("Red Cylinder").transform.rotation.eulerAngles.x;
+        float yAngle = GameObject.Find("Red Cylinder").transform.rotation.eulerAngles.y;
+        float zAngle = GameObject.Find("Red Cylinder").transform.rotation.eulerAngles.z;
+        //Debug.Log(yAngle);
+        //Debug.Log(xAngle);
+        //Debug.Log(yAngle);
+        //Debug.Log(zAngle);
+        //Debug.Log(GameObject.Find("left cap").transform.position);
+        //Debug.Log(GameObject.Find("Centre").transform.position.y);
+        if (isLeftGrounded && velocity.y < 0) {
+            velocity.y = -2f;
+            //Debug.Log("hello");
+        }
+        if (isRightGrounded && velocity.y < 0) {
+            velocity.y = -2f;
+        }
+        if (isCentreGrounded) {
+            Debug.Log("Game Over");
+        }
+        if (Input.GetKey("left") && isLeftGrounded) {
+            velocity.y = Mathf.Sqrt(bounce * -2f * gravity);
+            velocity.z = Mathf.Abs(bounce * Mathf.Cos(yAngle*Mathf.Deg2Rad));
+            velocity.x = Mathf.Abs(bounce * Mathf.Cos(zAngle*Mathf.Deg2Rad));
+            //Debug.Log(Mathf.Tan(Mathf.Abs(90 - yAngle)));
+            //Debug.Log(velocity.z);
+            //Debug.Log("left");
+        }
+        else if (Input.GetKey("right") && isRightGrounded) {
+            velocity.y = Mathf.Sqrt(bounce * -2f * gravity);
+            velocity.z = bounce * Mathf.Cos(yAngle*Mathf.Deg2Rad);
+            velocity.x = bounce * Mathf.Cos(zAngle*Mathf.Deg2Rad);
+        }
+        //Debug.Log(velocity.z);
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
