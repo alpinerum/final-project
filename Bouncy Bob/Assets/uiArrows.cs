814f6636ecaca58bd8089e5e4a9f46ad83f35388
alpinerum <35735448+alpinerum@users.noreply.github.com>
2020-04-22 22:17:53 -0400

    added ui elements

---
+++ Bouncy Bob/Assets/uiArrows.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class uiArrows : MonoBehaviour
+{
+    public GameObject leftGreenBox, rightGreenBox;
+    // Start is called before the first frame update
+    void Start()
+    {
+        leftGreenBox.SetActive(false);
+        rightGreenBox.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("left")) {
+            leftGreenBox.SetActive(true);
+            rightGreenBox.SetActive(false);
+        }
+        else if (Input.GetKeyDown("right")) {
+            leftGreenBox.SetActive(false);
+            rightGreenBox.SetActive(true);
+        }
+        if (!Input.anyKey) {
+            leftGreenBox.SetActive(false);
+            rightGreenBox.SetActive(false);
+        }
+    }
+}
